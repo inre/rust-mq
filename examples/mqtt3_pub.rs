e711ae5eac5e0e4dda9247deae8dfb71b87eed99
Maksim V <inre.storm@gmail.com>
2016-02-23 23:13:32 +0300

    Pub/Sub client examples using mqtt3 crate

---
+++ examples/mqtt3_pub.rs
@@ -0,0 +1,53 @@
+#![feature(convert)]
+extern crate mqtt3;
+
+use std::env;
+use std::net::TcpStream;
+use std::io::{self, Read, Write, BufReader, BufWriter};
+use std::process::exit;
+use mqtt3::{MqttRead, MqttWrite, Packet, Connect, Publish, Protocol, QoS, PacketIdentifier};
+use std::sync::Arc;
+
+fn main() {
+    let mut args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: crate run --example mqtt3_pub -- 127.0.0.1:1883");
+        exit(1);
+    }
+    let ref address = args[1];
+    println!("Establish connection to {}", address);
+    let mut stream = TcpStream::connect(address.as_str()).unwrap();
+    let mut reader = BufReader::new(stream.try_clone().unwrap());
+    let mut writer = BufWriter::new(stream.try_clone().unwrap());
+
+    // CONNECT -> CONNACK
+    let connect = Packet::Connect(Arc::new(Connect {
+        protocol: Protocol::MQTT(4),
+        keep_alive: 30,
+        client_id: "rust-mq-example-pub".to_owned(),
+        clean_session: true,
+        last_will: None,
+        username: None,
+        password: None
+    }));
+    println!("{:?}", connect);
+    writer.write_packet(&connect);
+    writer.flush();
+    let packet = reader.read_packet().unwrap();
+    println!("{:?}", packet);
+
+    // PUBLISH
+    let publish = Packet::Publish(Arc::new(Publish {
+        dup: false,
+        qos: QoS::AtLeastOnce,
+        retain: false,
+        topic_name: "/a/b".to_owned(),
+        pid: Some(PacketIdentifier(10)),
+        payload: Arc::new("Hello world".to_string().into_bytes())
+    }));
+    println!("{:?}", publish);
+    writer.write_packet(&publish);
+    writer.flush();
+    let packet = reader.read_packet().unwrap();
+    println!("{:?}", packet);
+}
---
+++ examples/mqtt3_sub.rs
@@ -0,0 +1,71 @@
+#![feature(convert)]
+extern crate mqtt3;
+
+use std::env;
+use std::net::TcpStream;
+use std::io::{self, Read, Write, BufReader, BufWriter};
+use std::process::exit;
+use mqtt3::{MqttRead, MqttWrite, Packet, Connect, Publish, Subscribe, Protocol, QoS, PacketIdentifier};
+use std::sync::Arc;
+
+fn main() {
+    let mut args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: crate run --example mqtt3_sub -- 127.0.0.1:1883");
+        exit(1);
+    }
+    let ref address = args[1];
+    println!("Establish connection to {}", address);
+    let mut stream = TcpStream::connect(address.as_str()).unwrap();
+    let mut reader = BufReader::new(stream.try_clone().unwrap());
+    let mut writer = BufWriter::new(stream.try_clone().unwrap());
+
+    // CONNECT -> CONNACK
+    let connect = Packet::Connect(Arc::new(Connect {
+        protocol: Protocol::MQTT(4),
+        keep_alive: 30,
+        client_id: "rust-mq-example-sub".to_owned(),
+        clean_session: true,
+        last_will: None,
+        username: None,
+        password: None
+    }));
+    println!("{:?}", connect);
+    writer.write_packet(&connect);
+    writer.flush();
+    let packet = reader.read_packet().unwrap();
+    println!("{:?}", packet);
+
+    // SUBSCRIBE
+    let subscribe = Packet::Subscribe(Arc::new(Subscribe {
+        pid: PacketIdentifier(260),
+        topics: vec![
+            ("/a/b".to_owned(), QoS::ExactlyOnce)
+        ]
+    }));
+    println!("{:?}", subscribe);
+    writer.write_packet(&subscribe);
+    writer.flush();
+    let packet = reader.read_packet().unwrap();
+    println!("{:?}", packet);
+
+    loop {
+        let packet = reader.read_packet().unwrap();
+        println!("{:?}", packet);
+
+        // PUBACK
+        match packet {
+            Packet::Publish(publish) => {
+                if publish.qos == QoS::AtLeastOnce {
+                    if let Some(pid) = publish.pid {
+                        let packet = Packet::Puback(pid);
+                        println!("{:?}", packet);
+                        writer.write_packet(&packet);
+                        writer.flush();
+                    }
+                }
+            },
+            _ => ()
+        }
+    }
+}
