b3394a911fc8c9a1cfa8467dff39f6716c86ae52
hmvp <github@hmvp.nl>
2016-07-26 17:27:38 +0200

    Implement std::error::Error and fmt::Display for Error types

--- mqttc/src/store.rs
+++ mqttc/src/store.rs
@@ -1,4 +1,6 @@
 use std::result;
+use std::error;
+use std::fmt;
 use mqtt3::{Message, PacketIdentifier};
 
 pub type Result<T> = result::Result<T, Error>;
@@ -14,4 +16,28 @@ pub trait Store {
 pub enum Error {
     NotFound(PacketIdentifier),
     Unavailable(PacketIdentifier)
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::NotFound(PacketIdentifier(packet_identifier)) =>
+                fmt::write(f, format_args!("Packet {} not found", packet_identifier)),
+            Error::Unavailable(PacketIdentifier(packet_identifier)) =>
+                fmt::write(f, format_args!("Packet {} unavailable", packet_identifier)),
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::NotFound(PacketIdentifier(_)) =>  "Packet not found",
+            Error::Unavailable(PacketIdentifier(_)) => "Packet unavailable",
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        None
+    }
 }
